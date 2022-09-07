dca50ca0f4b9ee56a093830231740e5807958249
Christopher LaChance <christopher.t.lachance@gmail.com>
2022-09-06 21:17:54 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
-#Install Powerlevel10k.
+#Install Powerlevel10k and theme.
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
-echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
+cat .zshrc > $HOME/.zshrc
+cat .p10k.zsh > $HOME/.p10k.zsh
+
+# Update time zone.
+sudo ln -fs /usr/share/zoneinfo/America/Chicago /etc/localtime
+sudo dpkg-reconfigure --frontend noninteractive tzdata

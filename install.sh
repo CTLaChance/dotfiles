5c044692ca4d952a76aa48f4d74eefc13326b5a9
Christopher LaChance <christopher.t.lachance@gmail.com>
2022-09-06 20:59:58 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,5 @@
 #!/usr/bin/env bash
 
-#Install fonts used by Powerlevel10k.
-sudo wget --directory-prefix=/usr/share/fonts https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf
-sudo wget --directory-prefix=/usr/share/fonts https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold.ttf
-sudo wget --directory-prefix=/usr/share/fonts https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Italic.ttf
-sudo wget --directory-prefix=/usr/share/fonts https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold%20Italic.ttf
-sudo fc-cache -f -v
-
 #Install Powerlevel10k.
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
 echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
